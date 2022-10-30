dcc1511da78a05ebc8ec4aadd9c2fec053803931
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-10-30 09:47:51 +0100

    add profile page

--- Backface_frontend/src/Pages/Home.jsx
+++ Backface_frontend/src/Pages/Home.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Navbar, SideBar, Posts } from '../Components/exports'
+import { SideBar, Posts } from '../Components/exports'
 
 const Home = () => {
   return (
-    <div className='overflow-hidden h-[100vh] w-[100vw] bg-[#E3E3E3]'>
-      <Navbar />
-      <div className='flex items-strech w-[100vw] h-[100vh] gap-2 px-1 pt-4'>
-        <div className='flex items-center justify-center w-1/4'>
-          <SideBar />
-        </div>
-        <div className='flex items-center justify-center w-2/4 px-8'>
-          <Posts />
-        </div>
-        <div className='flex items-center justify-center hidden md:block w-1/4'>
-          <SideBar />
+    <>
+      <div className='relative overflow-hidden h-[100vh] w-[100vw] bg-[#E3E3E3]'>
+        <div className='flex items-strech w-[100vw] h-[100vh] gap-10 px-1'>
+          <div className='flex items-center justify-center w-[400px]  '>
+            <SideBar />
+          </div>
+          <div className=' flex items-center justify-center w-[500px]  px-1 '>
+            <Posts />
+          </div>
+          <div className='flex items-center justify-center hidden md:block w-[400px]  '>
+            <SideBar />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- Backface_frontend/src/Pages/exports.js
+++ Backface_frontend/src/Pages/exports.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Login from "./Login";
 import Protector from "./Protector"
+import Profile from "./Profile";
 
 export {
  Home,
  Login,
+ Profile,
  Protector
 }
