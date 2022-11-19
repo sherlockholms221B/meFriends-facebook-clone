035099e72ae0d802fc7d6c21c3bab96082a216c2
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-11-19 07:17:47 +0100

    refactor home component

--- Backface_frontend/src/Pages/Home.jsx
+++ Backface_frontend/src/Pages/Home.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { SideBar, Posts } from '../Components/exports'
+import React, { useState } from 'react'
+import { SideBar, Posts, Advertisment } from '../Components/exports'
 
 const Home = () => {
   return (
     <>
       <div className='relative overflow-hidden h-[100vh] w-[100vw] bg-[#E3E3E3]'>
-        <div className='flex items-strech w-[100vw] h-[100vh] gap-10 px-1'>
-          <div className='flex items-center justify-center w-[400px]  '>
+        <div className='flex items-strech w-[100vw] h-[100vh] gap-24 px-1'>
+          <div className=' w-[350px]'>
             <SideBar />
           </div>
           <div className=' flex items-center justify-center w-[500px]  px-1 '>
             <Posts />
           </div>
-          <div className='flex items-center justify-center hidden md:block w-[400px]  '>
-            <SideBar />
+          <div className='flex items-center justify-center hidden md:block w-[400px]'>
+            <Advertisment />
           </div>
         </div>
       </div>
