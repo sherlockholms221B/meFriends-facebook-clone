bab831f73e9a9a6e35751bb804fcc8670c1de05f
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-11-20 03:59:21 +0100

    refactor

--- Backface_frontend/src/Pages/Home.jsx
+++ Backface_frontend/src/Pages/Home.jsx
@@ -4,9 +4,9 @@ import { SideBar, Posts, Advertisment } from '../Components/exports'
 const Home = () => {
   return (
     <>
-      <div className='relative overflow-hidden h-[100vh] w-[100vw] bg-[#E3E3E3]'>
-        <div className='flex items-strech w-[100vw] h-[100vh] gap-24 px-1'>
-          <div className=' w-[350px]'>
+      <div className='relative overflow-hidden h-[100vh] w-[100vw] pt-[60px] bg-[#E3E3E3]'>
+        <div className='flex items-strech w-[100vw] h-[100%] gap-24 px-1'>
+          <div className='overflow-auto home_scroll w-[350px]'>
             <SideBar />
           </div>
           <div className=' flex items-center justify-center w-[500px]  px-1 '>
