1e0fe5cdd85d42776e4113123f0cfcfb2154b276
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-11-22 01:25:52 +0100

    refactor

--- Backface_frontend/src/Components/Posts/CreatePost.jsx
+++ Backface_frontend/src/Components/Posts/CreatePost.jsx
@@ -1,10 +1,131 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  MdClose,
+  MdArrowBack,
+  MdArrowForward,
+  MdPerson,
+  MdOutlineDone,
+  MdDoneAll,
+  MdDownloadDone,
+  MdOutlineLocationOn,
+  MdFlag,
+  MdOutlinePhotoLibrary,
+} from 'react-icons/md'
+import { GiEarthAmerica } from 'react-icons/gi'
+import { HiDotsHorizontal, HiLockClosed } from 'react-icons/hi'
+import { FaUserFriends } from 'react-icons/fa'
+import { GoTriangleDown } from 'react-icons/go'
+import { BsEmojiWink } from 'react-icons/bs'
+
 import MegWrapper from '../MegWrapper'
+import { useGlobalContext } from '../../Context/UseContext'
+import { ProfilePicture } from '../index'
+import { previous, audience } from '../../utils/Functions'
 
 const CreatePost = () => {
+  const [postvalue, setPostvalue] = useState('')
+  const {
+    createPostState,
+    setCreatePostState,
+    addToYourState,
+    setAddToYourState,
+    postAudienceState,
+    setPostAudienceState,
+  } = useGlobalContext()
   return (
-    <div className=' hidden bg-[#E3E3E3] shadow-md rounded-md h-[70%] w-[40%] '>
-      CreatePost
+    <div className='bg-white z-20  w-full h-full rounded-lg'>
+      <div className='relative flex flex-row py-4 items-center justify-center border-b border-gray-300'>
+        <h3 className='text-xl font-bold text-gray-900'>Create post</h3>
+        <p
+          className='absolute top-2 right-2 rounded-full hover:bg-gray-400 bg-gray-300 p-2 cursor-pointer'
+          onClick={() => setCreatePostState(false)}
+        >
+          <MdClose className='text-2xl ' />
+        </p>
+      </div>
+      <div className='w-full h-full p-4'>
+        <div className='flex flex-row gap-4 items-center'>
+          <ProfilePicture />
+          <div>
+            <h3 className='text-md font-semibold text-gray-800 capitalize'>
+              Don christsanctus chinedu
+            </h3>
+            <div
+              className='flex flex-row gap-2 items-center rounded-sm bg-gray-200 w-min py-0.5 px-1 cursor-pointer '
+              onClick={() => audience(setCreatePostState, setPostAudienceState)}
+            >
+              <GiEarthAmerica />
+              <h4 className='text-sm text-gray-900 '>Public</h4>
+              <GoTriangleDown />
+            </div>
+          </div>
+        </div>
+        <input
+          type={'text'}
+          value={postvalue}
+          name={'postvalue'}
+          placeholder={"What's on your mind Don"}
+          onChange={(e) => setPostvalue(e.target.value)}
+          className={` w-full mt-2 mb-8 h-max  outline-none placeholder:text-2xl pt-4 pl-2 placeholder:text-gray-500`}
+        />
+        <div className='w-full flex flex-row justify-between items-center'>
+          <div className='cursor-pointer flex items-center justify-center py-1 px-1.5 rounded-md bg-blue-400'>
+            <p className='text-white text-xl text-gray-500'>Aa</p>
+          </div>
+          <BsEmojiWink className='cursor-pointer text-xl text-gray-500' />
+        </div>
+
+        <div className='w-full border-2 flex flex-row justify-between items-center p-3 mt-4 rounded-md'>
+          <h5 className='text-md font-semibold text-gray-800 '>
+            Add to your post
+          </h5>
+          <div className='flex flex-row gap-2 items-center'>
+            {[
+              { icon: <MdOutlinePhotoLibrary /> },
+              { icon: <MdPerson /> },
+              { icon: <BsEmojiWink /> },
+              {
+                icon: <MdOutlineLocationOn />,
+              },
+              {
+                icon: <MdFlag />,
+              },
+            ].map(({ icon }, i) => (
+              <p
+                className={`${i === 0 && 'text-green-600'} ${
+                  i === 1 && 'text-blue-600'
+                } ${i === 2 && 'text-yellow-600'} ${
+                  i === 3 && 'text-red-600'
+                } ${i === 4 && 'text-blue-400'} cursor-pointer text-2xl`}
+                key={i}
+              >
+                {icon}
+              </p>
+            ))}
+            <p className='text-xl text-gray-500b rounded-full p-1.5 hover:bg-gray-200 cursor-pointer'>
+              <HiDotsHorizontal />
+            </p>
+          </div>
+        </div>
+        {postvalue ? (
+          <button
+            type='button'
+            className={`w-full p-2 mt-2 mb-2 rounded-lg text-center 
+                bg-blue-500 text-white font-bold text-lg
+            `}
+          >
+            Post
+          </button>
+        ) : (
+          <button
+            type='button'
+            className={`w-full p-2 mt-2 mb-2 rounded-lg text-center bg-gray-200 text-gray-400 font-bold text-lg
+            `}
+          >
+            Post
+          </button>
+        )}
+      </div>
     </div>
   )
 }
--- Backface_frontend/src/Components/Posts/Post.jsx
+++ Backface_frontend/src/Components/Posts/Post.jsx
@@ -13,9 +13,7 @@ import { AiOutlineLike } from 'react-icons/ai'
 import { GoComment } from 'react-icons/go'
 import { RiShareForwardLine } from 'react-icons/ri'
 
-import { Comments, Input, PostOptions } from '../exports'
-import { comments } from '../../utils/constants'
-import { profile } from '../../Assets/exports'
+import { Comments, PostOptions, Like } from '../index'
 import { useGlobalContext } from '../../Context/UseContext'
 
 const Post = ({ isPrivate, profileImage, createdAt, creator, post }) => {
@@ -97,10 +95,7 @@ const Post = ({ isPrivate, profileImage, createdAt, creator, post }) => {
               isComment ? 'mx-2 border-b-2 border-gray-300 ' : ''
             }`}
           >
-            <div className='flex flex-row flex-nowrap justify-center  items-center hover:bg-[#E3E3E3] rounded-[3px] px-8 py-2 cursor-pointer '>
-              <AiOutlineLike className='text-xl font-bold text-gray-500 mr-1' />
-              <p className='text-md text-gray-500 font-semibold'>Like</p>
-            </div>
+            <Like />
             <div
               className='flex flex-row justify-center flex-nowrap items-center  hover:bg-[#E3E3E3] rounded-[3px] px-8 py-2 cursor-pointer'
               onClick={() => setIsComment(!isComment)}
@@ -114,88 +109,14 @@ const Post = ({ isPrivate, profileImage, createdAt, creator, post }) => {
             </div>
           </div>
 
-          <motion.div
-            className={`flex flex-col ${isComment && 'p-2'} justify-between `}
-          >
-            {isComment && (
-              <div className='flex flex-row flex-nowrap gap-2 items-center mb-4'>
-                <Link to={`/backface/api/profile`}>
-                  <div className=' rounded-full cursor-pointer'>
-                    <img
-                      src={profile}
-                      alt='profile'
-                      className='object-cover w-8 h-8 rounded-full'
-                    />
-                  </div>
-                </Link>
-                <Input
-                  handleChange={(e) => setValue(e.target.value)}
-                  value={value}
-                  type={`text`}
-                  name={`comment`}
-                  placeholder={`Write a comment`}
-                />
-              </div>
-            )}
-
-            {!viewMore
-              ? comments
-                  .slice(0, 1)
-                  .map(
-                    (
-                      {
-                        creator,
-                        profile: [{ profileImage, createdAt }],
-                        comment,
-                      },
-                      index
-                    ) => (
-                      <Comments
-                        isComment={isComment}
-                        setIsComment={setIsComment}
-                        createdAt={createdAt}
-                        creator={creator}
-                        profileImage={profileImage}
-                        comment={comment}
-                        key={`${creator + index}`}
-                      />
-                    )
-                  )
-              : comments.map(
-                  (
-                    {
-                      creator,
-                      profile: [{ profileImage, createdAt }],
-                      comment,
-                    },
-                    index
-                  ) => (
-                    <Comments
-                      isComment={isComment}
-                      setIsComment={setIsComment}
-                      createdAt={createdAt}
-                      creator={creator}
-                      profileImage={profileImage}
-                      comment={comment}
-                      key={`${creator + index}`}
-                    />
-                  )
-                )}
-            {isComment && (
-              <>
-                {!viewMore && (
-                  <motion.button
-                    whileHover={{ scale: [1, 1.1] }}
-                    transition={{ duration: 0.5, ease: 'easeInOut' }}
-                    onClick={() => setViewMore(!viewMore)}
-                    className='text-sm text-gray-500 font-medium self-start ml-14 mt-2'
-                  >
-                    see more
-                  </motion.button>
-                )}
-              </>
-            )}
-          </motion.div>
+          <Comments
+            isComment={isComment}
+            setIsComment={setIsComment}
+            viewMore={viewMore}
+            setViewMore={setViewMore}
+            value={value}
+            setValue={setValue}
+          />
         </div>
       </div>
     </>
