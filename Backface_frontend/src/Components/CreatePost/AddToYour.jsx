ccc68fe93305dea9d1ef4506a537a2b123447cb2
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-12-04 11:21:23 +0100

    add to createPost component

---
+++ Backface_frontend/src/Components/CreatePost/AddToYour.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+  MdClose,
+  MdArrowBack,
+  MdArrowForward,
+  MdPerson,
+  MdOutlineDone,
+  MdDoneAll,
+  MdDownloadDone,
+  MdOutlineLocationOn,
+  MdFlag,
+  MdOutlinePhotoLibrary,
+  MdVideoCall,
+} from 'react-icons/md'
+import { GiEarthAmerica } from 'react-icons/gi'
+import { HiDotsHorizontal, HiLockClosed } from 'react-icons/hi'
+import { FaUserFriends } from 'react-icons/fa'
+import { GoTriangleDown } from 'react-icons/go'
+import { BsEmojiWink } from 'react-icons/bs'
+
+import MegWrapper from '../MegWrapper'
+import { Head, CreatePostLinks } from '../index'
+import { useGlobalContext } from '../../Context/UseContext'
+import { forward, iconHandler, previous } from '../../utils/Functions'
+
+const AddToYour = () => {
+  const {
+    createPostState,
+    setCreatePostState,
+    addToYourState,
+    setAddToYourState,
+    postAudienceState,
+    setPostAudienceState,
+    setGoBack,
+  } = useGlobalContext()
+  return (
+    <div
+      className={` ${
+        addToYourState ? 'translate_x' : ''
+      } bg-white z-20  w-full h-full rounded-lg`}
+    >
+      <Head
+        title={`Add to your post`}
+        setFirstState={setCreatePostState}
+        setSecondState={setAddToYourState}
+        setThirdState={setGoBack}
+      />
+      <CreatePostLinks home={false} />
+    </div>
+  )
+}
+
+export default MegWrapper(AddToYour)
