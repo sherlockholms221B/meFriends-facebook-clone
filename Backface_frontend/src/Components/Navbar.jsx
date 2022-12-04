caac3c5b7c4ba392c122b72de8022161dcc32bca
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-12-04 11:29:56 +0100

    resolve bug

--- Backface_frontend/src/Components/Navbar.jsx
+++ Backface_frontend/src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { MdLink, MdNotifications } from 'react-icons/md'
+import { MdLink } from 'react-icons/md'
 import { MdOutlineExpandMore, MdOutlineSearch } from 'react-icons/md'
+
 import { profile } from '../Assets/exports'
-import { Input } from './index'
+import { Input, ToolTip } from './index'
 import { refresh } from '../utils/Functions'
-import { FaFacebookMessenger } from 'react-icons/fa'
+import { navLinks } from '../utils/links'
 
 const Navbar = () => {
   const [searchValue, setSearchValue] = useState('')
@@ -27,17 +28,10 @@ const Navbar = () => {
       </div>
 
       <div className='flex flex-row gap-2 items-center'>
-        {[
-          {
-            icon: <FaFacebookMessenger />,
-            alarts: '3',
-          },
-          {
-            icon: <MdNotifications />,
-            alarts: '1',
-          },
-        ].map(({ icon, alarts }, i) => (
+        {navLinks.map(({ icon, alarts, tip }, i) => (
           <div
+            data-tip
+            data-for={tip}
             className='relative p-[11px] rounded-full bg-[#E3E3E3]  hover:bg-[#D8D5D5] cursor-pointer '
             key={i + alarts}
           >
@@ -45,9 +39,18 @@ const Navbar = () => {
             <div className='flex items-center justify-center absolute w-[20px] h-[20px] rounded-full bg-red-600 top-0 right-0 border-2 border-white z-10'>
               <p className='text-white text-[12px] font-bold'>{alarts}</p>
             </div>
+            <ToolTip
+              title={tip}
+              id={tip}
+              effect={`float`}
+              type={`dark`}
+              place={`bottom`}
+            />
           </div>
         ))}
         <Link
+          data-tip
+          data-for='profile'
           to={`/backface/api/profile`}
           className='relative border-2 border-white rounded-full cursor-pointer'
         >
@@ -56,6 +59,13 @@ const Navbar = () => {
           <div className='absolute bottom-[-3px] right-0 rounded-full bg-[#E3E3E3]'>
             <MdOutlineExpandMore className='text-lg ' />
           </div>
+          <ToolTip
+            title={`account`}
+            effect={`float`}
+            type={`dark`}
+            place={`left`}
+            id={`profile`}
+          />
         </Link>
       </div>
     </nav>
