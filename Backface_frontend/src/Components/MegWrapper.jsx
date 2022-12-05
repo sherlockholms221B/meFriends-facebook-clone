fdafacc28a84b5af102275ff53e070106d13e714
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-12-05 10:30:50 +0100

    add minnor changes

--- Backface_frontend/src/Components/MegWrapper.jsx
+++ Backface_frontend/src/Components/MegWrapper.jsx
@@ -2,26 +2,40 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 import { useGlobalContext } from '../Context/UseContext'
+import NoPost from './NoPost'
 
 const MegWrapper = (Component) =>
   function HOC() {
     const {
       createPostState,
       addToYourState,
-      setAddToYourState,
-      setCreatePostState,
       postAudienceState,
+      tagPeopleState,
+      notificationState,
+      messageState,
     } = useGlobalContext()
     return (
       <>
-        {createPostState || addToYourState || postAudienceState ? (
-          <div className='flex justify-center items-center absolute top-0 backdrop-blur-lg right-0  z-10 h-screen w-screen'>
+        {createPostState ||
+        addToYourState ||
+        postAudienceState ||
+        tagPeopleState ? (
+          <div className='flex justify-center items-center absolute top-0 backdrop-blur-sm right-0  z-10 h-screen w-screen'>
             <div className='w-[500px] h-max bg-white rounded-lg shadow-black '>
               <Component />
             </div>
           </div>
         ) : (
-          <div className=''>No command found</div>
+          ''
+        )}
+        {messageState || notificationState ? (
+          <div className='flex justify-center items-center absolute top-10 backdrop-blur-sm right-10 z-10  bg-red-500'>
+            <div className='w-[500px] h-max bg-white  shadow-black '>
+              <Component />
+            </div>
+          </div>
+        ) : (
+          <NoPost title={`No chart found`} />
         )}
       </>
     )
