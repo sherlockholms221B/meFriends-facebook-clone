61b36a7a8602c2e5c2678b806f0938b40e2a46a1
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-12-06 10:39:01 +0100

    refactor

--- Backface_frontend/src/Components/Posts/ProfilePicture.jsx
+++ Backface_frontend/src/Components/Posts/ProfilePicture.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { profile } from '../../Assets/exports'
 
 const ProfilePicture = () => {
   return (
-    <Link
-      to={`/backface/api/profile`}
-      className='relative border-2 border-white rounded-full cursor-pointer'
-    >
+    <div className='relative border-2 border-white rounded-full cursor-pointer'>
       <img src={profile} alt='profile' className='object-cover w-10 h-10' />
-    </Link>
+    </div>
   )
 }
 
