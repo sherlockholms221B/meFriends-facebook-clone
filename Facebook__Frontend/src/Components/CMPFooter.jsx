3c1aeab6f14fbc7bdc058cf3f2bfecaa43d06512
Christsanctus chinedu <chineduchristsanctus020@gmail.com>
2022-12-19 18:27:38 +0100

    add custom footer

---
+++ Facebook__Frontend/src/Components/CMPFooter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CMPFooter = () => {
+  return (
+    <section className='mt-2 mb-1'>
+      <p className='text-sm'>
+        Privacy · Terms · Advertising · Ad choices · Cookies ·{' '}
+        <span className='cursor-pointer'>more</span> · Meta © 2022
+      </p>
+    </section>
+  )
+}
+
+export default CMPFooter
