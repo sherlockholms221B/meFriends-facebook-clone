55995c9486ebf61334b6bc42d1c25018644fb2d4
Christsanctus chinedu <103469283+sherlockholms221B@users.noreply.github.com>
2022-12-28 03:07:43 -0800

    delete backface dir and create facebook_frontend

--- Backface_frontend/src/Components/Advertisment.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Advertisment = () => {
-  return <div>Advertisment</div>
-}
-
-export default Advertisment
--- Backface_frontend/src/Components/Like.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Like = () => {
-  return <div>Like</div>
-}
-
-export default Like
