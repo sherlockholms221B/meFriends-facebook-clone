7d2eb4c97edee309bb712c8db391d5d1d3144cb4
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-01 09:04:51 -1100

    add dynamic menu component for groups

---
+++ Facebook__Frontend/src/Components/GPDT.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DynamicMenu from './DynamicMenu/DynamicMenu'
+
+const GPDT = () => {
+  return (
+    // Create Group,Shortcut and friends details box
+    <div className='text-red-500 p-10'>GPDT</div>
+  )
+}
+
+export default DynamicMenu(GPDT)
