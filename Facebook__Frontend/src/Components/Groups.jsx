0509d69ffe43b9b0c1182f7ba5ef888d5d43570a
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-01 09:04:04 -1100

    refactor

--- Facebook__Frontend/src/Components/Groups.jsx
+++ Facebook__Frontend/src/Components/Groups.jsx
@@ -3,13 +3,22 @@ import { MdGroups } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 import { groupsLinks } from '../utils/links'
-import Active from './Active'
+import { Active } from './index'
+import { useGlobalContext } from '../Context/UseContext'
 
 const Groups = ({ isMbar, barHarder, barContainer, roundedIcon }) => {
+  const { dynamicLocation } = useGlobalContext()
   return (
     <>
       {groupsLinks.map(({ img, name }) => (
         <Link
+          onMouseOver={(e) => {
+            const element = e.target.getBoundingClientRect()
+            dynamicLocation({
+              left: element.right,
+              center: (element.top + element.bottom) / 2,
+            })
+          }}
           to={`/backface/api/profile`}
           key={name}
           className={` ${
@@ -17,12 +26,14 @@ const Groups = ({ isMbar, barHarder, barContainer, roundedIcon }) => {
           } relative  larg:dark:hover:bg-darkComplementry larg:hover:bg-primary hover:bg-inherit  mb-0.5 flex flex-row gap-2 items-center w-full py-2 rounded-md`}
         >
           <img
+            onMouseOver={(e) => e.stopPropagation()}
             src={img}
             alt='profile'
             className='object-cover w-6 h-6 rounded-lg'
           />
           {isMbar && (
             <h3
+              onMouseOver={(e) => e.stopPropagation()}
               className={`dark:text-white hidden larg:block text-md font-semibold text-gray-900 `}
             >
               {name}
