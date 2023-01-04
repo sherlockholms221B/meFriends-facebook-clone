17c71d3e4c553893f3ece06acc164078e2ccf83b
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-04 10:56:00 -1100

    new comp.

---
+++ Facebook__Frontend/src/Controllers/ShortCutsSl.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import MenuPageUrl from './MenuPageUrl'
+const ShortCutsSl = () => {
+  return <div>ShortCutsSl</div>
+}
+
+export default MenuPageUrl(ShortCutsSl)
---
+++ Facebook__Frontend/src/Controllers/Watch.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import MenuPage from './MenuPage'
+
+const Watch = () => {
+  return (
+    <div className='dark:bg-darkSecondary w-full h-full flex items-center justify-center'>
+      <h4 className='text-white text-3xl'> Watch Component </h4>
+    </div>
+  )
+}
+
+const WatchBtn = () => {
+  return (
+    <div className=''>
+      <h4 className='text-white text-3xl'> WatchBtn </h4>
+    </div>
+  )
+}
+
+export default MenuPage(Watch, WatchBtn, 'watch')
