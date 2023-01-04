0b2f9780c56716dd928ec6e4b9169eeb469eec26
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-04 10:51:51 -1100

    export controllers

---
+++ Facebook__Frontend/src/Controllers/index.js
@@ -0,0 +1,19 @@
+import Friends from './Friends'
+import Saved from './Saved'
+import Memories from './Memories'
+import Watch from './Watch'
+import MarketPlace from './MarketPlace'
+import MenuSl from './MenuSL'
+import GroupSl from './GroupSl'
+import ShortCutsSl from './ShortCutsSl'
+
+export {
+  Friends,
+  Saved,
+  Memories,
+  MarketPlace,
+  Watch,
+  MenuSl,
+  ShortCutsSl,
+  GroupSl,
+}
