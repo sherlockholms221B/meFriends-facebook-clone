132293dc6587630ce572f93129120f53854a8785
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-04 10:54:16 -1100

    create Menu side navigation bar controller

---
+++ Facebook__Frontend/src/Controllers/MenuSL.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import MenuPageUrl from './MenuPageUrl'
+
+const MenuSL = () => {
+  return <section className=''>menusl</section>
+}
+
+export default MenuPageUrl(MenuSL)
