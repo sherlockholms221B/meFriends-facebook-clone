05621c80d2d4b0adbb62ee7111c06a866b1c6755
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-04 13:21:53 -1100

    style friends componet

--- Facebook__Frontend/src/Controllers/Friends.jsx
+++ Facebook__Frontend/src/Controllers/Friends.jsx
@@ -1,19 +1,135 @@
 import React from 'react'
 
 import MenuPage from './MenuPage'
+import { Icon } from '../utils/Icon'
+import { comments } from '../utils/constants'
 
 const Friends = () => {
   return (
-    <div className='dark:bg-darkSecondary w-full h-full flex items-center justify-center'>
-      <h4 className='text-white text-3xl'> Friends Component </h4>
+    <div className='dark:bg-darkPrimary flex flex-col gap-4 w-full h-full p-4'>
+      <div className='flex flex-row justify-between items-center p-2 pt-4 pb-0 '>
+        <h5 className='text-2xl text-gray-800 dark:text-heading_dark_white font-bold'>
+          People you may know
+        </h5>
+        <p className=' text-xl rounded-full  dark:bg-darkComplementry dark:text-heading_dark_white bg-gray-200 p-2 w-fit h-fit'>
+          <Icon.MdSettings />
+        </p>
+      </div>
+      <section className='flex flex-row flex-wrap w-full gap-3 pl-2 pr-2 group'>
+        {comments.map(
+          ({ creator, profile: [{ profileImage }], isPrivate }, i) => (
+            <div
+              key={i + creator}
+              className='w-[180px] h-[320px] flex-grow dark:bg-darkSecondary rounded-xl'
+            >
+              <img
+                src={profileImage}
+                alt=''
+                className='w-full h-1/2 object-cover rounded-t-xl'
+              />
+              <div className='h-1/2 w-full flex flex-col items-center justify-evenly text-left'>
+                <div className='w-[90%] flex flex-col gap-1 items-start justify-evenly '>
+                  <h4 className='text-gray-900 font-medium dark:text-white capitalize'>
+                    {creator}
+                  </h4>
+                  <div className='flex flex-row gap-2 items-center justify-start'>
+                    <div className='flex flex-row'>
+                      {comments.map(({ profile: [{ profileImage }] }, i) => (
+                        <img
+                          src={profileImage}
+                          key={i}
+                          alt='friends'
+                          className={`w-3 h-3 rounded-full  ${
+                            i === 0 && 'left-0 '
+                          } ${i === 1 && 'left-8'}   ${
+                            i > 1 && 'hidden'
+                          } ring-2 ring-white
+                           `}
+                        />
+                      ))}
+                    </div>
+                    <p className='text-xs dark:text-heading_dark_gray'>{`20 mutual friends`}</p>
+                  </div>
+                </div>
+                <button className='bg-secondaryWhite hover:bg-primaryWhite dark:bg-blue-600 dark:text-heading_dark_white py-2 text-lg rounded-lg font-semibold w-[90%]'>
+                  Add friend
+                </button>
+                <button className='bg-secondaryWhite hover:bg-primaryWhite dark:bg-darkComplementry dark:text-heading_dark_white py-2 text-lg rounded-lg font-semibold w-[90%]'>
+                  Remove
+                </button>
+              </div>
+            </div>
+          )
+        )}
+      </section>
     </div>
   )
 }
 
 const FriendsBtn = () => {
   return (
-    <div className=''>
-      <h4 className='text-white text-3xl'> FriendsBtn </h4>
+    <div className='flex flex-col gap-4 w-full h-full '>
+      <div className='flex flex-row justify-between items-center p-2 '>
+        <h5 className='text-2xl text-gray-800 dark:text-heading_dark_white font-bold'>
+          Friends
+        </h5>
+        <p className=' text-xl rounded-full  dark:bg-darkComplementry dark:text-heading_dark_white bg-gray-200 p-2 w-fit h-fit'>
+          <Icon.MdSettings />
+        </p>
+      </div>
+      <section className='w-full flex flex-col gap-1 pl-2 pr-2 group'>
+        {[
+          {
+            icon: <Icon.RiSettings2Fill />,
+            title: 'Home',
+            isMove: false,
+          },
+          {
+            icon: <Icon.MdHttps />,
+            title: 'Friend request',
+            isMove: true,
+          },
+          {
+            icon: <Icon.AiOutlineBug />,
+            title: 'Suggestions ',
+            isMove: true,
+          },
+          {
+            icon: <Icon.MdOutlineFormatListBulleted />,
+            title: 'All Friends',
+            isMove: true,
+          },
+          {
+            icon: <Icon.MdTune />,
+            title: 'Birthdays',
+            isMove: false,
+          },
+          {
+            icon: <Icon.BsGlobe />,
+            title: 'Custome lists',
+            isMove: true,
+          },
+        ].map(({ icon, title, isMove, ref }, i) => (
+          <div
+            key={i + title}
+            className={`  ${title} flex justify-between items-center dark:hover:bg-darkComplementry hover:bg-secondaryWhite p-2 w-full rounded-md cursor-pointer first:bg-darkComplementry first:hover:bg-darkComplementry `}
+          >
+            <div className='flex flex-row items-center gap-2 w-fit '>
+              <p className=' text-xl rounded-full  dark:bg-darkComplementry dark:text-heading_dark_white bg-gray-200 p-2 w-fit h-fit'>
+                {icon}
+              </p>
+              <h4 className='text-gray-900 font-medium dark:text-white'>
+                {title}
+              </h4>
+            </div>
+            {isMove && (
+              <p className='font-medium text-2xl dark:text-white'>
+                <Icon.MdOutlineArrowForwardIos />
+              </p>
+            )}
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
--- Facebook__Frontend/src/Controllers/MenuPage.jsx
+++ Facebook__Frontend/src/Controllers/MenuPage.jsx
@@ -17,23 +17,23 @@ const MenuPage = (Component, ComponentBtn, idName) =>
         <Navbar />
         <div
           style={{ paddingTop: `${location.height}px` }}
-          className='flex items-center justify-center dark:bg-darkPrimary relative overflow-auto h-[100vh] w-[100vw] bg-primary'
+          className='flex items-center justify-center dark:bg-darkPrimary relative h-screen w-screen bg-primary overflow-hidden'
         >
           <div className=' flex items-strech mid_small:justify-between w-[100vw] h-[100%]'>
             <MSideBar />
-            <div className=' min-w-[260px] w-full overflow-auto h-full relative'>
+            <div className=' min-w-[260px] w-full  h-full relative'>
               <div className='flex_row w-screen h-screen '>
                 <motion.div
                   whileInView={{ opacity: [0, 1] }}
                   transition={{ duration: 1, ease: 'easeInOut' }}
-                  className='w-550 dark:bg-darkSecondary h-full overflow-hidden flex items-center justify-center border-r-2 dark:border-borderDark'
+                  className='w-550 dark:bg-darkSecondary h-full overflow-hidden flex items-start justify-center border-r-2 dark:border-borderDark'
                 >
                   {idName && <ComponentBtn />}
                 </motion.div>
                 <motion.div
                   whileInView={{ opacity: [0, 1], x: [200, 0] }}
                   transition={{ duration: 0.5, easing: 'easeInOut' }}
-                  className='w-full bg-black h-full text-white relative flex justify-center'
+                  className='w-full bg-black h-full text-white relative flex justify-center overflow-auto'
                 >
                   {(query === 'Friends' ||
                     query === 'Saved' ||
