e803e7d4cb0e084bd0e31e362fda2db44eb2b8ab
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-17 13:27:15 +0100

    write efficent code

--- Facebook__Frontend/src/Components/Posts/IMG.jsx
+++ Facebook__Frontend/src/Components/Posts/IMG.jsx
@@ -8,11 +8,11 @@ const IMG = ({ post, postlength }) => {
         to={`/post/details/photo?fbid=${`544657569769743763856776056734384697367458468744835`}`}
       >
         {' '}
-        <div className='h-full w-[100%] border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+        <div className='h-full w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
           <img
             src={post[0]}
             alt='post'
-            className='h-full w-[100%] object-cover'
+            className='h-full w-full object-cover'
           />
         </div>
       </Link>
@@ -23,22 +23,18 @@ const IMG = ({ post, postlength }) => {
       <Link
         to={`/post/details/photo?fbid=${`544657569769743763856776056734384697367458468744835`}`}
       >
-        <div className='flex flex-row gap-0.5 h-458 w-[100%] border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
-          <div className='h-full w-1/2'>
-            <img
-              src={post[0]}
-              alt='post'
-              className='h-full w-full object-cover'
-            />
-          </div>
-          <div className='h-full w-1/2'>
-            {' '}
-            <img
-              src={post[1]}
-              alt='post'
-              className='h-full w-full object-cover'
-            />
-          </div>
+        <div className='flex flex-row gap-0.5 h-458 w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+          {post.map((post, i) => {
+            return (
+              <div key={i} className='h-full w-1/2'>
+                <img
+                  src={post}
+                  alt='post'
+                  className='h-full w-full object-cover'
+                />
+              </div>
+            )
+          })}
         </div>
       </Link>
     )
@@ -48,31 +44,28 @@ const IMG = ({ post, postlength }) => {
       <Link
         to={`/post/details/photo?fbid=${`544657569769743763856776056734384697367458468744835`}`}
       >
-        <div className='flex flex-row gap-0.5 h-458 w-[100%] border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+        <div className='flex flex-row gap-0.5 h-458 w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
           <div className='h-full w-1/2'>
             <img
               src={post[0]}
               alt='post'
-              className='h-full w-[100%] object-cover'
+              className='h-full w-full object-cover'
             />
           </div>
-          <div className='h-full w-1/2'>
-            <div className='flex flex-col gap-0.5 h-full w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
-              <div className='h-1/2 w-full'>
-                <img
-                  src={post[1]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='h-1/2 w-full'>
-                <img
-                  src={post[2]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-            </div>
+          <div className='h-full w-1/2 flex flex-col gap-0.5 border-gray-300 dark:border-[#3a3b3c]'>
+            {post
+              .filter((post, i) => i !== 0)
+              .map((post, i) => {
+                return (
+                  <div className='h-1/2 w-full'>
+                    <img
+                      src={post}
+                      alt='post'
+                      className='h-full w-full object-cover'
+                    />
+                  </div>
+                )
+              })}
           </div>
         </div>
       </Link>
@@ -83,38 +76,28 @@ const IMG = ({ post, postlength }) => {
       <Link
         to={`/post/details/photo?fbid=${`544657569769743763856776056734384697367458468744835`}`}
       >
-        <div className='flex flex-row gap-0.5 h-458 w-[100%] border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+        <div className='flex flex-row gap-0.5 h-458 w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
           <div className='h-full w-2/3'>
             <img
               src={post[0]}
               alt='post'
-              className='h-full w-[100%] object-cover'
+              className='h-full w-full object-cover'
             />
           </div>
-          <div className='h-full w-[44%]'>
-            <div className='flex flex-col gap-0.5 h-full w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
-              <div className='h-[33%] w-full'>
-                <img
-                  src={post[1]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='h-[33%] w-full'>
-                <img
-                  src={post[2]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='h-[33%] w-full'>
-                <img
-                  src={post[3]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-            </div>
+          <div className='h-full w-[44%] flex flex-col gap-0.5 border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+            {post
+              .filter((post, i) => i !== 0)
+              .map((post, i) => {
+                return (
+                  <div className='h-[33%] w-full'>
+                    <img
+                      src={post}
+                      alt='post'
+                      className='h-full w-full object-cover'
+                    />
+                  </div>
+                )
+              })}
           </div>
         </div>
       </Link>
@@ -125,50 +108,44 @@ const IMG = ({ post, postlength }) => {
       <Link
         to={`/post/details/photo?fbid=${`544657569769743763856776056734384697367458468744835`}`}
       >
-        <div className='flex flex-row gap-0.5 h-458 w-[100%] border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+        <div className='flex flex-row gap-0.5 h-458 w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
           <div className='h-full w-2/3'>
             <img
               src={post[0]}
               alt='post'
-              className='h-full w-[100%] object-cover'
+              className='h-full w-full object-cover'
             />
           </div>
-          <div className='h-full w-[44%]'>
-            <div className='flex flex-col gap-0.5 h-full w-full border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
-              <div className='h-[33%] w-full'>
-                <img
-                  src={post[1]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='h-[33%] w-full'>
-                <img
-                  src={post[2]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='h-[33%] w-full relative'>
-                <img
-                  src={post[3]}
-                  alt='post'
-                  className='h-full w-full object-cover'
-                />
-                <div className='flex items-center justify-center absolute top-0 right-0 w-full h-full bg-transparent'>
-                  <h3 className='text-4xl text-white'>{`+${
-                    postlength - 4
-                  }`}</h3>
-                </div>
-              </div>
-            </div>
+          <div className='h-full w-[44%] flex flex-col gap-0.5 border-b-2 border-t-2 border-gray-300 dark:border-[#3a3b3c]'>
+            {post
+              .filter((post, i) => i >= 4)
+              .map((post, i) => {
+                return (
+                  <div className='h-[33%] w-full relative'>
+                    <img
+                      src={post}
+                      alt='post'
+                      className='h-full w-full object-cover'
+                    />
+                    <article
+                      className={`${
+                        i !== 2 ? 'hidden' : 'flex'
+                      } flex items-center justify-center absolute top-0 right-0 w-full h-full bg-transparent`}
+                    >
+                      <h3 className='text-4xl text-white'>{`+${
+                        postlength - 4
+                      }`}</h3>
+                    </article>
+                  </div>
+                )
+              })}
           </div>
         </div>
       </Link>
     )
   }
 
-  return <div className=''>chicken boririro</div>
+  return <section className=''>Something went wrong check back letter</section>
 }
 
 export default IMG
