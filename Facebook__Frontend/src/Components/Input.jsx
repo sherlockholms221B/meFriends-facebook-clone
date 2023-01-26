78667badbf57a44eead666d40177434cb447c9ef
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-01-26 22:56:33 +0100

    refactor menusl

--- Facebook__Frontend/src/Components/Input.jsx
+++ Facebook__Frontend/src/Components/Input.jsx
@@ -19,7 +19,9 @@ const Input = ({ type, value, name, placeholder, handleChange }) => {
         } ${
           name === 'friends' &&
           'pl-10 py-1.5 pr-4 mid_small:w-[410px] xtra_small:w-260 w-210 placeholder:text-md'
-        } ${name === 'comment' && ' w-10/12 px-2 py-1 placeholder:text-sm'}  ${
+        } ${name === 'menusl' && 'pl-10 py-1.5 w-full placeholder:text-md'} ${
+          name === 'comment' && ' w-10/12 px-2 py-1 placeholder:text-sm'
+        }  ${
           name === 'chats' &&
           'w-11/12 pl-10 py-1.5 pr-4 placeholder:text-md placeholder:font-normal '
         } outline-none  bg-primary dark:bg-darkComplementry hover:bg-secondaryWhite rounded-full  placeholder:text-gray-600 dark:placeholder:text-heading_dark_gray`}
