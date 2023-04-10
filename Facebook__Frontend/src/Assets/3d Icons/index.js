a2264d881a5d0980a4946a34c032f2deb4d07743
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-04-10 06:51:19 +0100

    add static bundle assests

---
+++ Facebook__Frontend/src/Assets/3d Icons/index.js
@@ -0,0 +1,23 @@
+import bag from './bag-dynamic-gradient.png'
+import book from './bookmark-dynamic-gradient.png'
+import bookmark from './bookmark-fav-dynamic-gradient.png'
+import clock from './clock-dynamic-gradient.png'
+import video from './video-camera-dynamic-gradient.png'
+import friend from './girl-dynamic-gradient.png'
+import gift from './gift-dynamic-gradient.png'
+import camera from './camera-dynamic-gradient.png'
+import picture from './picture-dynamic-gradient.png'
+import mic from './mic-dynamic-gradient.png'
+
+export const DIcons = {
+  bag,
+  book,
+  bookmark,
+  clock,
+  video,
+  friend,
+  gift,
+  camera,
+  picture,
+  mic,
+}
