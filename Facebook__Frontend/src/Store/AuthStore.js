41edd10e57c51b16743e700a82173284afd4d884
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-04-13 19:13:54 +0100

    zustand store

--- Facebook__Frontend/src/Store/AuthStore.js
+++ Facebook__Frontend/src/Store/AuthStore.js
@@ -1,6 +1,6 @@
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
-import axios from 'axios'
+// import axios from 'axios'
 
 const authStore = (set) => ({
   userProfile: null,
