ed04164b92ce029adf21d0da2ba3a9e0021a42ff
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-06 07:11:48 +0100

    set client environment variables

--- Facebook__Backend/Clients/index.js
+++ Facebook__Backend/Clients/index.js
@@ -2,12 +2,12 @@ import { createClient } from "@sanity/client";
 import ImageUrl from "@sanity/image-url";
 
 export const client = new createClient({
-  projectId: "projectId",
-  token: "token",
-  dataset: "project dataset",
-  apiVersion: "api version",
+  projectId: process.env.SANITY_PROJECT_ID,
+  token: process.env.SANITY_PROJECT_TOKEN,
+  dataset: 'project dataset',
+  apiVersion: '2023-03-09',
   useCdn: false,
-});
+})
 
 const builder = ImageUrl(client);
 
