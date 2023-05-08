b9d768a1700b66194d9ec51c416f67211b856db0
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-08 07:39:40 +0100

    add match media

--- Facebook__Frontend/src/Controllers/MenuPage.jsx
+++ Facebook__Frontend/src/Controllers/MenuPage.jsx
@@ -10,18 +10,30 @@ import { useLocation } from 'react-router-dom'
 import { useGlobalContext } from '../Hooks/context/UseContext'
 
 //import components
+import { mNavLink } from '../utils/links'
 import { Navbar } from '../Components'
 import MSideBar from '../Components/SideBars/MSideBar'
+import MbarLink from '../Components/Navigation Bar/components/SoftMobileNav'
 
 const MenuPage = (Component, ComponentBtn, idName) =>
   function HOC() {
+    const WILL_ANIMATE = window.matchMedia('(min-with:700px)').matches
     const search = useLocation().search
     const query = new URLSearchParams(search).get('talling')
-     const { location } = useGlobalContext()
+    const { location } = useGlobalContext()
 
     return (
       <React.Fragment>
-        <Navbar />
+        {WILL_ANIMATE ? (
+          <Navbar />
+        ) : (
+          <section className='dark:bg-dark400 bg-white flex flex-row items-center w-full justify-between px-4 py-2 mdsm:hidden'>
+            {mNavLink.map((data, index) => (
+              <MbarLink {...data} key={index} />
+            ))}
+          </section>
+        )}
+
         <section
           style={{ marginTop: `${location.height}px` }}
           className='flex items-center justify-center relative h-screen w-screen overflow-hidden'
@@ -30,14 +42,20 @@ const MenuPage = (Component, ComponentBtn, idName) =>
             <MSideBar />
             <section className='min-w-[260px] w-full h-full relative flex flex-col md:flex-row items-center bg-light500 dark:bg-dark500'>
               <motion.div
-                whileInView={{ opacity: [0, 1] }}
+                whileInView={
+                  WILL_ANIMATE ? { opacity: [0, 1] } : { opacity: [0.99, 1] }
+                }
                 transition={{ duration: 1, ease: 'easeInOut' }}
                 className='md:w-550 w-full dark:bg-dark500 md:dark:bg-dark400 bg-white h-fit md:h-full overflow-hidden flex items-start justify-start md:justify-center md:border-r-2 md:dark:border-bd500'
               >
                 {idName && <ComponentBtn />}
               </motion.div>
               <motion.div
-                whileInView={{ opacity: [0, 1], x: [200, 0] }}
+                whileInView={
+                  WILL_ANIMATE
+                    ? { opacity: [0, 1], x: [200, 0] }
+                    : { opacity: [0.99, 1] }
+                }
                 transition={{ duration: 0.5, easing: 'easeInOut' }}
                 className='w-full h-full md:h-full bg-light500 dark:bg-dark500 text-white relative flex justify-center overflow-auto'
               >
