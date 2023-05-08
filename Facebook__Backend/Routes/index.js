eb47b80e56c2ab62a2a6b7107bc01562b89617d0
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-08 07:33:32 +0100

    refactor

--- Facebook__Backend/Routes/index.js
+++ Facebook__Backend/Routes/index.js
@@ -1,22 +1,26 @@
 import express from "express";
 import {
   getPostRoute,
-  makePostRoute,
+  getSinglePost,
+  createPostRoute,
   likePostRoute,
   makeComentRoute,
   deletePostRoute,
   getProduct,
   createProduct,
-} from "../Controllers/index.js";
+} from '../Controllers/index.js'
 
-const router = express.Router();
+const router = express.Router()
 
 //post routes
-router.get("/facebook-clone-modern/get-all-post", getPostRoute);
-router.post("/facebook-clone-modern/create-post", makePostRoute);
-router.put("/facebook-clone-modern/like-post", likePostRoute);
-router.post("/facebook-clone-modern/comment-on-post", makeComentRoute);
-router.delete("/facebook-clone-modern/delete-post", deletePostRoute);
+router
+  .get('/facebook-clone-modern/get-all-post', getPostRoute)
+  .get('/facebook-clone-modern/get-single-post', getSinglePost)
+router
+  .post('/facebook-clone-modern/create-one-post', createPostRoute)
+  .post('/facebook-clone-modern/comment-on-post', makeComentRoute)
+router.delete('/facebook-clone-modern/delete-post', deletePostRoute)
+router.put('/facebook-clone-modern/like-post', likePostRoute)
 //shoping routes
 router.get("/facebook-clone-modern/get-all/product/home", getProduct);
 router.post("/facebook-clone-modern/create-product", createProduct);
