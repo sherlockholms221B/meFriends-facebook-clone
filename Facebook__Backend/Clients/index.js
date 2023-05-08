624a40f82ab6858ed5f7952193d199deec4017af
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-08 07:31:12 +0100

    add sanity client configuration

--- Facebook__Backend/Clients/index.js
+++ Facebook__Backend/Clients/index.js
@@ -1,14 +1,19 @@
-import { createClient } from "@sanity/client";
-import ImageUrl from "@sanity/image-url";
+//the integration of sanity cms
+//the file holds the configuration required for validation and accessing of data
+//from sanity cms
+//Don't change anything if you don't knw what you are doing.
 
-export const client = new createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
+import { createClient } from '@sanity/client'
+import ImageUrl from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: 'nuhfhosf',
   token: process.env.SANITY_PROJECT_TOKEN,
-  dataset: 'project dataset',
+  dataset: 'production',
   apiVersion: '2023-03-09',
   useCdn: false,
 })
 
-const builder = ImageUrl(client);
+const builder = ImageUrl(client)
 
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source) => builder.image(source)
