77924310df85c21dae34bca9de8e930588511141
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-08 17:52:41 +0100

    patch

--- Facebook__Backend/Clients/index.js
+++ Facebook__Backend/Clients/index.js
@@ -8,7 +8,8 @@ import ImageUrl from '@sanity/image-url'
 
 export const client = createClient({
   projectId: 'nuhfhosf',
-  token: process.env.SANITY_PROJECT_TOKEN,
+  token:
+    'skoqXVa2VwL2HM7G0kAcIJaQ59dLgMUo3wxaeZbo6bK30QwOUdE7mNZE1IPzuYfV3ABJGxMDQKo5TxZQd5K7dXbi25ahSpICoGtfp9N5nbzyyc1GB2JI314YPmqt4yFhJVFJ6Hb1OEe0VAasayjkgoxsgdOou0Mlyif2RYFIBqVJy7SxaKWe',
   dataset: 'production',
   apiVersion: '2023-03-09',
   useCdn: false,
