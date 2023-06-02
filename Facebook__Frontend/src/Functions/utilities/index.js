1f556f5746d0a19467e35ea9be40eecfeae0c5ae
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-02 12:05:46 +0100

    .

--- Facebook__Frontend/src/Functions/utilities/index.js
+++ Facebook__Frontend/src/Functions/utilities/index.js
@@ -51,3 +51,43 @@ export function post(data) {
     dispatchCall({ type: GET_SINGLE_POST, payload: response })
   })
 }
+
+export const kMeans = (data, k = 1) => {
+  const centroids = data.slice(0, k)
+  const distances = Array.from({ length: data.length }, () =>
+    Array.from({ length: k }, () => 0)
+  )
+  const classes = Array.from({ length: data.length }, () => -1)
+  let itr = true
+
+  while (itr) {
+    itr = false
+
+    for (let d in data) {
+      for (let c = 0; c < k; c++) {
+        distances[d][c] = Math.hypot(
+          ...Object.keys(data[0]).map((key) => data[d][key] - centroids[c][key])
+        )
+      }
+      const m = distances[d].indexOf(Math.min(...distances[d]))
+      if (classes[d] !== m) itr = true
+      classes[d] = m
+    }
+
+    for (let c = 0; c < k; c++) {
+      centroids[c] = Array.from({ length: data[0].length }, () => 0)
+      const size = data.reduce((acc, _, d) => {
+        if (classes[d] === c) {
+          acc++
+          for (let i in data[0]) centroids[c][i] += data[d][i]
+        }
+        return acc
+      }, 0)
+      for (let i in data[0]) {
+        centroids[c][i] = parseFloat(Number(centroids[c][i] / size).toFixed(2))
+      }
+    }
+  }
+
+  return classes
+}
