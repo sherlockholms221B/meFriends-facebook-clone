ce7b453c5876fdaf4a257b7698abeb0c5358ef2a
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-02 12:17:06 +0100

    create ai models

---
+++ Facebook__Backend/Models/Utils/Functions.js
@@ -0,0 +1,10 @@
+//reuseable functtions
+class suggestion extends Function {
+  constructor(props) {
+    super(props);
+    const _RFACTOR = function linkingRecomedsations() {
+      console.log('functions');
+    };
+  }
+}
+export default suggestion;
