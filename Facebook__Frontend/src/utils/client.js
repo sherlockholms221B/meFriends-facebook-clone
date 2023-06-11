b3fef698fb35b637bc3c66c954ab941f82185309
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-11 03:03:56 +0100

    config

---
+++ Facebook__Frontend/src/utils/client.js
@@ -0,0 +1,13 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+  projectId: 'nuhfhosf',
+  dataset: 'production',
+  token: process.env.SANITY_PROJECT_TOKEN,
+  useCdn: false,
+  apiVersion: '2022-09-20',
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
