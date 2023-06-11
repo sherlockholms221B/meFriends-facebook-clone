f87e6d137381909ae79e34730c139a3745df200a
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-11 14:39:33 +0100

    update

--- Facebook__Frontend/src/socket.js
+++ Facebook__Frontend/src/socket.js
@@ -1,8 +1,11 @@
 //import socket.io from 'socket.io';
 import { io } from 'socket.io-client'
 
+
 //declere url string
+// const url = process.env.REACT_APP_BASE_URL;
 const url = 'http://localhost:8080'
+console.log(typeof process.env.REACT_APP_BASE_URL);
 
 const socket = io(url)
 
