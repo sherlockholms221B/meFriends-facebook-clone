dd1e0769b8238d01fca031197efc5807fb5db810
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-17 01:33:36 +0100

    mini sixing

---
+++ Facebook__Backend/Models/Data/python/data/Scraper.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import time,tensorflow as tf
+from array import *
+
+""" 
+needed urls:
+
+1: Amazon prime news: https://www.amazon.com/s?k=news&ref=nb_sb_noss
+2:Jumia Glass data: https://www.jumia.com.ng/catalog/?q=glass
+
+"""
+
