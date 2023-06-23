9e2be5930faa8bc2e9753342dbeefd81e9c64118
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-23 21:01:36 +0100

    update sounds

--- Facebook__Frontend/src/Assets/Audio/index.js
+++ Facebook__Frontend/src/Assets/Audio/index.js
@@ -1,8 +1,6 @@
-import boing from './Slack - Boing.mp3'
-import ding from './Slack - Ding.mp3'
-import drop from './Slack - Drop.mp3'
-import here from './Slack - Here you go.mp3'
-import hi from './Slack - Hi.mp3'
-import like from './Slack - Wow.mp3'
+import likeSound from './MEFRIENDS-LIKE.mp3';
+import commentSound from './MEFRIEND-COMMENT.mp3';
+import errorSound from './MEFIRENDS-ERROR.mp3';
+import messageSound from './MEFRIENDS-MESSAGE.mp3';
 
-export { boing, ding, drop, here, hi, like }
+export { likeSound, commentSound, errorSound, messageSound };
