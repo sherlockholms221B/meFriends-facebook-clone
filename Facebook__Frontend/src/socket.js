67074efbe420e88aeff879e719f631ee7d26affa
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-06-23 21:03:07 +0100

    .

--- Facebook__Frontend/src/socket.js
+++ Facebook__Frontend/src/socket.js
@@ -1,12 +1,14 @@
 //import socket.io from 'socket.io';
-import { io } from 'socket.io-client'
-
+import { io } from 'socket.io-client';
 
 //declere url string
-// const url = process.env.REACT_APP_BASE_URL;
-const url = 'http://localhost:8080'
+const url = process.env.REACT_APP_BASE_URL;
+// const url = 'http://localhost:8080';
 console.log(typeof process.env.REACT_APP_BASE_URL);
 
-const socket = io(url)
+const socket = io('https://app-server-mefriends.vercel.app', {
+  forceNew: true,
+  transports: ['polling'],
+});
 
-export default socket
+export default socket;
