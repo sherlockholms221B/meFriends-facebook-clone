86768cb6a3265db9cd220a78afe81da1877428e2
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-05 11:59:47 +0100

    hext

--- Facebook__Backend/index.js
+++ Facebook__Backend/index.js
@@ -10,17 +10,18 @@ require('dotenv').config();
 //   },
 // });
 
+const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: 'http://localhost:3001',
-  },
-});
+// const io = socketIO(server, {
+//   cors: {
+//     origin: 'http://localhost:3001',
+//   },
+// });
 const cors = require('cors');
 
 //import routes from routes
@@ -30,12 +31,8 @@ const chatRoutes = require('./Routes/chatRoute.js');
 
 //middlewares
 const { errorHandler, notFound } = require('./Middleware/errorMiddleware.js');
-app.use(
-  cors({
-    origin: 'http://localhost:3001',
-    credentials: true, // if you need to send cookies or authorization headers
-  })
-); // Enable CORS for all routes
+
+app.use(cors()); // Enable CORS for all routes
 
 app.use(express.json());
 
@@ -43,7 +40,6 @@ app.get('/api/data', (req, res) => {
   // Handle the request and send a response
   res.json({ message: 'Hello from the server!' });
 });
-
 
 //routes
 app.use('/facebook-clone-modern', postRoutes);
@@ -87,23 +83,31 @@ app.use('/facebook-clone-modern/api/message', messageRoutes);
 //   });
 // });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
 
-  // Handle when a client sends a message
-  socket.on('chat message', (msg) => {
-    console.log('Message received: ' + msg);
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
-  });
+//   // Handle when a client sends a message
+//   socket.on('chat message', (msg) => {
+//     console.log('Message received: ' + msg);
+//     io.emit('chat message', msg); // Broadcast the message to all connected clients
+//   });
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+//   // Handle disconnection
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// For any other route, serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //declear port or take in the process.env
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
 
