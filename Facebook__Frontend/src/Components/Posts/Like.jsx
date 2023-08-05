04922ad411db149f69e4e40e9ab5ab5f3f399e4d
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-05 01:53:50 +0100

    like functionality

--- Facebook__Frontend/src/Components/Posts/Like.jsx
+++ Facebook__Frontend/src/Components/Posts/Like.jsx
@@ -9,43 +9,46 @@ import { Icon } from '../../utils/Icon';
 
 //
 
-export const LikeVW = (rtnlikes) => {
-  const [likes, setlikes] = React.useState([]);
-
-  // React.useEffect(() => {
-  //   console.log(rtnlikes.length);
-  // }, [rtnlikes]);
-
+export const LikeVW = ({ likes }) => {
   return (
-    <>
-      {rtnlikes.length !== 0 && (
-        <article className='flex items-center justify-center'>
-          <Icon.AiOutlineLike className='mr-1 text-blue-500' />
-          <p className='text-sm text-gray-500 '>{rtnlikes.length}</p>
-        </article>
+    <article className='flex items-center justify-center'>
+      <Icon.AiOutlineLike className='mr-1 text-blue-500' />
+      {likes?.length !== 0 && (
+        <p className='text-sm text-gray-500 '>{likes?.length}</p>
       )}
-    </>
+    </article>
   );
 };
 
-export const LikeBTN = ({ likes, postId }) => {
+export const LikeBTN = ({ like, postId }) => {
   const { userProfile } = useAuthStore();
+  const [color, setColor] = React.useState('');
   //
   const {
-    internalAction: [controller, dispatchAction],
     externalAction: [state, dispatchCall],
   } = useGlobalContext();
   //
-  React.useEffect(() => {}, []);
-  //
+
+  React.useEffect(() => {
+    // Find the specific post in the state by postId
+    const post = state.find((post) => post._id === postId);
+
+    // Check if the post exists and if the user has liked it
+    if (post?.likes.some((like) => like._ref === userProfile?._id)) {
+      setColor('text-blue-500'); // User has liked the post, change color to blue
+    } else {
+      setColor('dark:text-thlight500 text-gray-500'); // User hasn't liked the post, remove the color
+    }
+  }, [state, postId, userProfile]);
+
   return (
     <button
-      onClick={(event) => {
+      onClick={() => {
         likePost(dispatchCall, { userId: userProfile?._id, postId });
       }}
       className='flex flex-row flex-nowrap justify-center  items-center sm:dark:hover:bg-dark300 sm:hover:bg-light500 bg-light500 dark:bg-dark300 sm:dark:bg-inherit sm:rounded-[3px] rounded-full px-6 xs:px-8 py-2 cursor-pointer '
     >
-      <AiOutlineLike className='text-xl font-bold text-gray-500 mr-1  dark:text-thlight500' />
+      <AiOutlineLike className={`text-xl font-bold mr-1  ${color}`} />
       <p className='hidden sm:block text-sm xs:text-md text-gray-500 font-semibold dark:text-thlight500'>
         Like
       </p>
