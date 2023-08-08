c3b012f6e973bf7887b5f13dc3ed3021304d82a4
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-08 03:10:36 +0100

    seperate socket script

--- Facebook__Backend/index.js
+++ Facebook__Backend/index.js
@@ -1,27 +1,14 @@
 //.env's
 require('dotenv').config();
 
-// const express = require('express');
-// const app = express();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http, {
-//   cors: {
-//     origin: 'http://localhost:3001',
-//   },
-// });
-
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const socketServer = require('./Socket/socket.js');
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketIO(server, {
-//   cors: {
-//     origin: 'http://localhost:3001',
-//   },
-// });
+const io = socketServer(server);
 const cors = require('cors');
 
 //import routes from routes
@@ -50,54 +37,6 @@ app.use('/facebook-clone-modern/api/message', messageRoutes);
 // app.use(notFound);
 // app.use(errorHandler);
 
-// io.on('connection', (socket) => {
-//   console.log('Connected to socket.io');
-//   socket.on('setup', (userData) => {
-//     socket.join(userData._id);
-//     socket.emit('connected');
-//   });
-
-//   socket.on('join chat', (room) => {
-//     socket.join(room);
-//     // console.log("User Joined Room: " + room);
-//   });
-//   socket.on('typing', (room) => socket.in(room).emit('typing'));
-//   socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
-
-//   socket.on('new message', ({ data, senderId }) => {
-//     // var chat = data?.message
-//     const ppl = data?.whoIsIn_id;
-
-//     if (!ppl.length || ppl.length === 1) return; //console.log("chat.users not defined");
-
-//     ppl.forEach((user) => {
-//       if (user?._ref == senderId) return; //return if the user is the sender.
-
-//       socket.in(user?._ref).emit('message recieved', data); //dispatch emit action
-//     });
-//   });
-
-//   socket.off('setup', () => {
-//     // console.log("USER DISCONNECTED");
-//     socket.leave(userData._id);
-//   });
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-
-//   // Handle when a client sends a message
-//   socket.on('chat message', (msg) => {
-//     console.log('Message received: ' + msg);
-//     io.emit('chat message', msg); // Broadcast the message to all connected clients
-//   });
-
-//   // Handle disconnection
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-// });
-
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -110,5 +49,3 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
-
-
