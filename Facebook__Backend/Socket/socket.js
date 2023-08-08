db5d3eb7bebb345ed0bce1e0b3569f61a4a07b81
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-08 03:11:21 +0100

    socket dir

---
+++ Facebook__Backend/Socket/socket.js
@@ -0,0 +1,51 @@
+// socketServer.js
+
+const socketIO = require('socket.io');
+
+module.exports = (server) => {
+  const io = socketIO(server, {
+    //allow cors for dev url.
+    cors: {
+      origin: 'http://localhost:3000',
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Connected to socket.io');
+
+    socket.on('setup', (userData) => {
+      console.log(userData._id);
+      socket.join(userData._id);
+      socket.emit('connected');
+    });
+
+    socket.on('join chat', (room) => {
+      socket.join(room);
+      console.log('User Joined Room: ' + room);
+    });
+
+    // socket.on('typing', (room) => socket.in(room).emit('typing'));
+    // socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
+    socket.on('new message', ({ data, senderId }) => {
+      // var chat = data?.message
+      const ppl = data?.whoIsIn_id;
+      console.log(ppl, 'ref', senderId, 'senderId');
+
+      if (!ppl.length || ppl.length === 1) return; //console.log("chat.users not defined");
+
+      ppl.forEach((user) => {
+        if (user?._ref == senderId) return; //return if the user is the sender.
+
+        socket.in(user?._ref).emit('message recieved', data); //dispatch emit action
+      });
+    });
+
+    socket.on('disconnect', () => {
+      console.log('USER DISCONNECTED');
+      socket.leave(userData._id);
+    });
+  });
+
+  return io;
+};
