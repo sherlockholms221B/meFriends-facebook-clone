f0f29acfdb675ab6afbbba75ecf272eddf08584c
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-08 03:12:54 +0100

    auth

--- Facebook__Backend/Middleware/authMiddleware.js
+++ Facebook__Backend/Middleware/authMiddleware.js
@@ -32,10 +32,10 @@ const protect = asyncHandler(async (req, res, next) => {
     }
   }
 
-  // if (!token) {
-  //   res.status(401)
-  //   throw new Error('Not authorized, no token')
-  // }
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
 });
 
 module.exports = protect;
