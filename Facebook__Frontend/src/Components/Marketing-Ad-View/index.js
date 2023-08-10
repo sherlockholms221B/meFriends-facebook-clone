50e42723ccc3d7b170b17340d518bc17635e6e97
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-10 04:17:38 +0100

    dir change

---
+++ Facebook__Frontend/src/Components/Marketing-Ad-View/index.js
@@ -0,0 +1,5 @@
+//main components
+export { default as ProductAdvertisement } from './Promotion-Banner/ProductAdvertisement'
+
+//attachements
+export {default as SponsoredContent} from './mini-components/SponsoredContent'
