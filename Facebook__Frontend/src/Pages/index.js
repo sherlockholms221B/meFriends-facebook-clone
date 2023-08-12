9176d2c5c2e1ecab08e0353a7fd375249f9dea66
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-08-12 02:54:24 +0100

    modify imports

--- Facebook__Frontend/src/Pages/index.js
+++ Facebook__Frontend/src/Pages/index.js
@@ -1,31 +1,5 @@
-import Home from './Home'
-import Login from './Login/Login.jsx'
-import Protector from './Protector'
-import Profile from './Profile/Profile'
-import PostDetails from './Posts/PostDetails'
-import STPage from './STPage'
-import EDIOP from './Profile/EDIOP'
-import Friends from './Profile/Friends'
-import Photos from './Profile/Photos'
-import HED from './Profile/HED'
-import PSTIMG from './Posts/PSTIMG'
-import { FriendStack } from '../Components/RDOMC'
-import StoryDetails from './StoryDetails/index'
-import IMG from '../Components/Posts/IMG'
+import Protector from './Protector';
+import { FriendStack } from '../Components/RDOMC';
+import IMG from '../Components/Posts/IMG';
 
-export {
-  Home,
-  Login,
-  Profile,
-  Protector,
-  PostDetails,
-  STPage,
-  EDIOP,
-  Friends,
-  Photos,
-  HED,
-  IMG,
-  PSTIMG,
-  FriendStack,
-  StoryDetails,
-}
+export { Protector, IMG, FriendStack };
